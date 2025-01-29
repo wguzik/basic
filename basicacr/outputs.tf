9a235b7b447b8f9fdcdd65d009955dad1c013648
wguzik <wojciech.guzik@gmail.com>
2025-01-29 22:15:03 +0100

    Update outputs.tf

--- basicacr/outputs.tf
+++ basicacr/outputs.tf
@@ -6,6 +6,10 @@ output "acr_password" {
   value = module.acr.acr_admin_password
 }
 
+output "acr_login_server" {
+  value = module.acr.acr_login_server
+}
+
 output "kv_name" {
   value = module.keyvault.key_vault_name
 }
