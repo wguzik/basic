8475dbb6b31a630246cc5a2fc56fb64892949432
wguzik <wojciech.guzik@gmail.com>
2025-01-31 20:57:30 +0100

    Update outputs.tf

--- basicwebapp/outputs.tf
+++ basicwebapp/outputs.tf
@@ -2,3 +2,8 @@ output "webapp_url" {
   description = "The URL of the web app"
   value       = module.webapp.webapp_url
 }
+
+output "rg_name" {
+  description = "Resource group name"
+  value       = module.resource_group.name
+}
